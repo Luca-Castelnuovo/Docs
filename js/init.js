5aff9ef4758976c9eff1b22c74d156afc547c272
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-12-29 19:37:22 +0100

    update

--- js/init.js
+++
@@ -1,8 +0,0 @@
-//Mobile Side nav
-document.addEventListener("DOMContentLoaded", function() {
-    var e = document.querySelectorAll(".sidenav");
-    M.Sidenav.init(e, {
-        edge: "right",
-        draggable: !0
-    })
-});
