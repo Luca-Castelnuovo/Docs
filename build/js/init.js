f7b84ddaec74414398a695497169578f860107a8
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-12-29 19:50:39 +0100

    update

--- build/js/init.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-//Mobile Side nav
-document.addEventListener("DOMContentLoaded",function(){var e=document.querySelectorAll(".sidenav");M.Sidenav.init(e,{edge:"right",draggable:!0})});
-//# sourceMappingURL=init.js.map
